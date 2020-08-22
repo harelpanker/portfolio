93f22ed10efb518a4d02bc31f2caacd825f8a233
Harel <harelpanker@gmail.com>
2020-08-22 09:04:09 +0200

    play with skills gap

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -2,13 +2,13 @@ import styles from "../styles/skills.module.scss";
 import skills from "../public/skills";
 import Flicking from "@egjs/react-flicking";
 import { AutoPlay } from "@egjs/flicking-plugins";
-import useWindowDimensions from "../hooks/useWindowDimensions";
+// import useWindowDimensions from "../hooks/useWindowDimensions";
 
 const flickingPlugins = [new AutoPlay(2000, "NEXT")];
 
 const Skills = () => {
-  const { width } = useWindowDimensions();
-  const gap = width > 768 ? 30 : 10;
+  // const { width } = useWindowDimensions();
+  // const gap = width > 768 ? 30 : 10;
 
   return (
     <section className={styles.section}>
@@ -16,7 +16,7 @@ const Skills = () => {
       <Flicking
         className={styles.flicking}
         circular={true}
-        gap={gap}
+        gap={30}
         duration={500}
         plugins={flickingPlugins}
         moveType='freeScroll'>
