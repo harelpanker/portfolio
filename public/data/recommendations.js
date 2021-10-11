e3e0c9d69972e7c3507778163eebb33333d17179
harelpanker <harelpanker@gmail.com>
2021-10-11 20:54:07 +0200

    added recommendation

--- public/data/recommendations.js
+++ public/data/recommendations.js
@@ -17,6 +17,14 @@ const recommendations = [
     image: '/images/recommendations/shimon.png',
     linkedinUrl: 'https://www.linkedin.com/in/tolts/',
   },
+  {
+    id: uuid(),
+    body: 'I have worked with Harel as a freelance developer at Antidote Health. He processes a great deal of knowledge and experience in the field of development, and always does extensive research when investigating if something can be done. Harel has great communication skills and professionalism and is always quick and efficient when completing projects. He has been a great pleasure to work with and I highly recommend him to any company looking to partner with him in development.',
+    name: 'Amanda Elizabeth Cohen',
+    position: 'Marketing Coordinator at Antidote Health',
+    image: '/images/recommendations/elizabeth.jpeg',
+    linkedinUrl: 'https://www.linkedin.com/in/amandaelizabethcrum/',
+  },
 ];
 
 export default recommendations;
