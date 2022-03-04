ab19d292f8dfca83b6478f08ad332fcb6b1d4d15
harelpanker <harelpanker@gmail.com>
2022-03-04 15:47:47 +0100

    adding gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,10 @@
-import { useEffect } from 'react';
-import Script from 'next/script';
-import { useRouter } from 'next/router';
-import { GA_TRACKING_ID, pageview } from '../lib/gtm';
-
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import Layout from '../components/layout/Layout';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    router.events.on('routeChangeComplete', pageview);
-    return () => {
-      router.events.off('routeChangeComplete', pageview);
-    };
-  }, [router.events]);
-
   return (
     <>
-      {/* Google Tag Manager - Global base code */}
-      <Script
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GA_TRACKING_ID}');`,
-        }}
-      />
       <Layout className='relative z-50'>
         <Component {...pageProps} />
       </Layout>
