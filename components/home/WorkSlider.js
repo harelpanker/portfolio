fc2691bf5099a6ca0efa93438684b1cfbcae551e
harelpanker <harelpanker@gmail.com>
2022-03-19 16:22:11 +0100

    Fixing home slider

--- components/home/WorkSlider.js
+++ components/home/WorkSlider.js
@@ -6,6 +6,7 @@ const WorkSlider = () => {
   return (
     <div className='container'>
       <Splide
+        hasAutoplayProgress
         options={{
           type: 'loop',
           drag: 'free',
@@ -14,11 +15,14 @@ const WorkSlider = () => {
           arrows: false,
           pagination: false,
           rewind: true,
-          gap: '1vw',
+          gap: '3vw',
           autoplay: true,
           pauseOnHover: true,
           pauseOnFocus: true,
           dragMinThreshold: true,
+          classes: { pagination: 'splide__pagination splide_pagination' },
+          interval: 5000,
+          speed: 1200,
         }}>
         {projectsData.map((proj) => (
           <SplideSlide
@@ -28,27 +32,25 @@ const WorkSlider = () => {
               href={proj.url}
               target='_blank'
               rel='noopener noreferrer'
-              className='relative rounded-xl overflow-hidden group duration-500 hover:-translate-y-4'>
-              <div className='max-h-[76vh] max-w-[100%] w-[1440px] relative'>
-                <Image
-                  src={proj.image}
-                  alt={proj.title}
-                  layout='responsive'
-                  blurDataURL
-                  placeholder='blur'
-                />
+              className='relative z-10 rounded-xl overflow-hidden group duration-500 hover:-translate-y-4'>
+              <div className='max-h-[76vh] max-w-[100%] w-[1440px] relative z-10'>
+                <Image src={proj.image} alt={proj.title} layout='responsive' />
               </div>
               {/* overlay */}
               <div className='absolute w-full h-full inset-0 top-0 left-0 right-0 bottom-0 bg-gray-800 opacity-50 z-20 transition-all duration-500 group-hover:opacity-30'></div>
               {/* content */}
-              <div className='absolute inset-0 p-3 sm:p-5 md:p-12 w-full h-full flex justify-start items-end z-50'>
+              <div className='absolute flex-col inset-0 p-3 sm:p-5 md:p-12 w-full h-full flex justify-end items-start z-30'>
                 <h3 className='text-gray-50 text-lg sm:text-xl md:text-4xl font-bold'>
                   {proj.title}
                 </h3>
+                <div className='text-xs md:text-base'>View live -&gt;</div>
               </div>
             </a>
           </SplideSlide>
         ))}
+        <div className='splide__progress'>
+          <div className='splide__progress__bar'></div>
+        </div>
       </Splide>
     </div>
   );
